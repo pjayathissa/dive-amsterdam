7d348b72bf067d797828b064d9de96f8d1423017
pjayathissa <p.jayathissa@gmail.com>
2020-05-31 19:55:39 +1200

    first itteration of new website

---
+++ js/client.js
@@ -0,0 +1,33 @@
+function countDownTimer() {
+  // Set the date we're counting down to
+  var countDownDate = new Date("Jan 1, 2099 15:37:25").getTime();
+
+  // Update the count down every 1 second
+  var x = setInterval(function() {
+
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var years = Math.floor(distance / (1000 * 60 * 60 * 24 * 365))
+    var days = Math.floor((distance % (1000 * 60 * 60 * 24 * 365))/ (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    document.getElementById("countdown").innerHTML = years + 'years ' + days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    // If the count down is finished, write some text
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+}
+
+countDownTimer()
